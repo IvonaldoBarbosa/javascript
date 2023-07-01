e86dd9398f14fa10adb1d86a4e9dec1c3aaaa2ef
Ivonaldo Barbosa de Freitas <133446115+IvonaldoBarbosa@users.noreply.github.com>
2023-07-01 13:43:21 -0300

    Atualização de codigo

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,20 +3,24 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var minuto = data.getMinutes()
     //var hora = 10
 
-    msg.innerHTML = (`Agora são ${hora} horas`)
+    msg.innerHTML = (`Agora são exatamente ${hora} horas e ${minuto} minutos`)
     if(hora >= 0 && hora < 12 ) {
         //Bom Dia
         img.src = 'manha.png'
         document.body.style.background = '#ddd496'
+        
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde
         img.src = 'tarde.png'
         document.body.style.background = '#f28d6a'
+        
     } else  {
         //Boa noite
         img.src = 'noite.png'
         document.body.style.background = '#053862'
+        
     }
 }
