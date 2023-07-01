cc77a996842fa0c0de534003523444f37b349512
Ivonaldo Barbosa de Freitas <133446115+IvonaldoBarbosa@users.noreply.github.com>
2023-07-01 15:22:52 -0300

    Modificando

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -4,23 +4,31 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     var minuto = data.getMinutes()
-    //var hora = 10
-
+    var res = window.document.getElementById('res')
+    
     msg.innerHTML = (`Agora são exatamente ${hora} horas e ${minuto} minutos`)
-    if(hora >= 0 && hora < 12 ) {
+    if(hora >= 5 && hora < 12 ) {
         //Bom Dia
         img.src = 'manha.png'
         document.body.style.background = '#ddd496'
+        res.innerHTML = 'Bom Dia!'
         
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde
         img.src = 'tarde.png'
         document.body.style.background = '#f28d6a'
+        res.innerHTML = 'Boa Tarde!'
         
-    } else  {
+    } else  if (hora >= 18 && hora <= 23) {
         //Boa noite
         img.src = 'noite.png'
         document.body.style.background = '#053862'
+        res.innerHTML = 'Boa Noite!'
         
+    } else {
+        //Boa Madrugada
+        img.src = 'noite.png'
+        document.body.style.background = '#053862'
+        res.innerHTML = 'Boa Madrugada!'
     }
 }
