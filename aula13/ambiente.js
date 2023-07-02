6101b5a89cd297636ceedc7b6e2fe7eea10d20a1
Ivonaldo Barbosa de Freitas <133446115+IvonaldoBarbosa@users.noreply.github.com>
2023-07-02 18:47:57 -0300

    Adicionando arquivos do curso

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+ //Etrutura com teste lógico no inico
+ /* var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+} */ 
+
+//Estrutura com teste lógico no final
+var c = 1
+ do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
